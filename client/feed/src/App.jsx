644608630f1be610f2fe3f72b3e24dcb80e5fef8
nainascode <nainadugar.official@gmail.com>
2026-02-22 00:14:44 +0530

    Switch to HashRouter for GitHub Pages

--- client/feed/src/App.jsx
+++ client/feed/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Create from './pages/Create';
 import Feed from './pages/Feed';
 import './App.css';
